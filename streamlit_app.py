8d3adf85325a0255ad40cd2d4fe61dced5ac181a
dsivaranjani <46396619+dsivaranjani@users.noreply.github.com>
2022-12-12 15:46:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My First streamlit Python code')
+streamlit.title('My Parents Healthy Breakfast')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
